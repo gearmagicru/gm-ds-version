58437b14ffc8705d2dd2a3a603cc6780f28e0057
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-10 11:53:12 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Этот файл является частью виджета информационной панели (Dashboard).
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'System version',
+    '{description}' => 'System version information',
+
+    // Options: поля
+    'Web application' => 'Web application',
+    'Edition' => 'Edition',
+    'Control Panel' => 'Control Panel',
+    'Short name' => 'Short name',
+    'Show sections' => 'Show sections',
+    'Show section items' => 'Show section items',
+    'Version' => 'Version',
+    'Version number' => 'Version number',
+    'Release date' => 'Release date',
+    'Details' => 'Details',
+    'Framework' => 'Framework',
+    '[unknown]' => '[unknown]'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Этот файл является частью виджета информационной панели (Dashboard).
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Версия системы',
+    '{description}' => 'Информация о версии системы',
+
+    // Options: поля
+    'Web application' => 'Веб-приложение',
+    'Edition' => 'Редакция',
+    'Control Panel' => 'Панель управления',
+    'Short name' => 'Короткое название',
+    'Show sections' => 'Показывать разделы',
+    'Show section items' => 'Показывать пункты разделов',
+    'Version' => 'Версия',
+    'Version number' => 'Номер версии',
+    'Release date' => 'Дата выпуска',
+    'Details' => 'Подробнее',
+    'Framework' => 'Фреймворк',
+    '[unknown]' => '[неизвестно]'
+];
